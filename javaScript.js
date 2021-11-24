c1f9607e9689a4de27aabf71309fe533b5b5652e
ligonw <89856445+ligonw@users.noreply.github.com>
2021-11-24 12:02:08 -0500

    Contact us page creation

---
+++ javaScript.js
@@ -0,0 +1,49 @@
+document.addEventListener("DOMContentLoaded", function() {
+ fields.firstName = document.getElementById('firstName');
+ fields.lastName = document.getElementById('lastName');
+ fields.email = document.getElementById('email');
+  fields.question = document.getElementById('question');
+})
+function isNotEmpty(value) {
+ if (value == null || typeof value == 'undefined' ) return false;
+ return (value.length > 0);
+}
+function fieldValidation(field, validationFunction) {
+ if (field == null) return false;
+
+ let isFieldValid = validationFunction(field.value)
+ if (!isFieldValid) {
+ field.className = 'placeholderRed';
+ } else {
+ field.className = '';
+ }
+
+ return isFieldValid;
+}
+function isValid() {
+ var valid = true;
+ 
+ valid &= fieldValidation(fields.firstName, isNotEmpty);
+ valid &= fieldValidation(fields.lastName, isNotEmpty);
+ valid &= fieldValidation(fields.email, isNotEmpty);
+  valid &= fieldValidation(fields.question, isNotEmpty);
+
+ return valid;
+}
+class User {
+ constructor(firstName, lastName, gender, address, country, email, newsletter, question) {
+ this.firstName = firstName;
+ this.lastName = lastName;
+  this.email = email;
+  this.question = question;
+ }
+}
+function sendContact() {
+  if (isValid()) {
+    let usr = new User (firstName.value, lastName.value, email.value);
+    
+    alert(`${usr.firstName} Thanks for your message, we will be in touch soon.`)
+  } else {
+    alert("There was an error, please try again later")
+  }
+  }
